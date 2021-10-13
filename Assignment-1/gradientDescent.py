db7d2be2b4a8804d538e0dd6a9b82bb125060fbe
CyberKnight1803 <omkarpitale2001@gmail.com>
2021-10-13 19:34:14 +0530

    add SGD

--- Assignment-1/gradientDescent.py
+++ Assignment-1/gradientDescent.py
@@ -12,7 +12,7 @@ class BatchGD(GD):
     def __init__(self, lRate):
         super().__init__(lRate=lRate)
     
-    def __call__(self, X, y, model, itr, costs, print_cost=True):
+    def __call__(self, X, y, model, itr, print_cost=True):
         m = X.shape[1]
         y_p = np.dot(model.W.T, X) + model.b 
         model.dW = (1 / m) * np.dot(X, y_p.T - y)
@@ -21,31 +21,32 @@ class BatchGD(GD):
         self.update(model)
 
         cost = model.compute_cost(X, y)
-        costs.append(cost)
+        model.costs.append(cost)
 
         if print_cost and itr % 50 == 0:
             print(f"Cost after iteration {itr + 1} : {cost}")
 
-class stochasticGD(GD):
+class StochasticGD(GD):
     def __init__(self, lRate=0.01):
         super().__init__(lRate=lRate)
     
-    def __call__(self, X, y, model, costs, itr, print_cost=True):
+    def __call__(self, X, y, model, itr, print_cost=True):
         m = X.shape[1]
-        k = y.shape[0]
 
         final_epoch_cost = 0
         for i in range(0, m):
             y_p = np.dot(model.W.T, X[:, i].reshape(-1, 1))
 
-            model.dW = np.dot(X[:, i], y_p.T - y[:, i].reshape(k, -1))
-            model.db = np.sum(y_p.T - y[:, i].reshape(k, -1))
+            model.dW = np.dot(X[:, i].reshape(-1, 1), y_p.T - y[i, :].reshape(1, -1))
+            model.db = np.sum(y_p.T - y[i, :].reshape(1, -1))
 
             self.update(model)
 
-            cost = model.compute_cost(X[:, i].reshape(-1, 1), y[:, i].reshape(k, -1))
-            costs.append(cost)
-            final_epoch_cost = cost 
+            cost = model.compute_cost(X[:, i].reshape(-1, 1), y[i, :].reshape(1, -1))
+            model.costs.append(cost)
+            final_epoch_cost = cost
+        
+        # model.costs.append(final_epoch_cost)
         
         if print_cost:
             print(f"Cost after epoch {itr + 1}: {final_epoch_cost}")
@@ -53,6 +54,6 @@ class stochasticGD(GD):
 
 GD_Variants = {
     'BatchGD': BatchGD,
-    'StochasticGD': stochasticGD
+    'StochasticGD': StochasticGD
 }
 
